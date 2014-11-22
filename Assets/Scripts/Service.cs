e0050c379b1ba903923a1898d35eab317c7c3fbb
Max Yankov <golergka@gmail.com>
2014-11-23 00:50:32 +0200

    Simple service provider

---
+++ Assets/Scripts/Service.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Service : MonoBehaviour
+{
+	protected virtual void Awake()
+	{
+		ServiceProvider.Register(this);
+	}
+}
---
+++ Assets/Scripts/ServiceProvider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ServiceProvider
+{
+	static Dictionary<Type,Service> m_Services = new Dictionary<Type,Service>();
+
+	public static T GetService<T>()
+		where T : Service
+	{
+		var type = typeof(T);
+		if (!m_Services.ContainsKey(type))
+		{
+			var go = new GameObject("_" + type.ToString());
+			var service = go.AddComponent<T>();
+			m_Services[type] = service;
+		}
+		return (T) m_Services[type];
+	}
+
+	public static void Register(Service _Service)
+	{
+		m_Services[_Service.GetType()] = _Service;
+	}
+}
