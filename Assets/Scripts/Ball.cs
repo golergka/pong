c58cee8c86b7c29350e799c755d2ae475d04693e
Max Yankov <golergka@gmail.com>
2014-11-22 23:29:36 +0200

    Ball reflects of the wall

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+
+	public Vector3 StartVelocity;
+
+	Vector3 m_Velocity = new Vector3();
+
+	public void Awake()
+	{
+		m_Velocity = StartVelocity;
+	}
+
+	public void OnCollisionEnter(Collision _Collision)
+	{
+		Debug.Log("Collision!");
+		m_Velocity.x = - m_Velocity.x;
+		m_Velocity.y = - m_Velocity.y;
+		m_Velocity.z = - m_Velocity.z;
+	}
+
+	public void FixedUpdate()
+	{
+		transform.position += m_Velocity * Time.deltaTime;
+	}
+
+}
