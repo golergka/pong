3ce0a8f0373f36e4d17f0ca7848d8035a8153686
Max Yankov <golergka@gmail.com>
2014-11-23 13:16:55 +0200

    Limit platform movement to field bounds

---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Field : Service
+{
+	public Bounds Bounds
+	{
+		get
+		{
+			return collider.bounds;
+		}
+	}
+}
--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -5,11 +5,35 @@ public class Platform : MonoBehaviour
 {
 	public float Speed;
 
+	Field m_Field;
+	Field Field
+	{
+		get
+		{
+			if (m_Field == null)
+			{
+				m_Field = ServiceProvider.GetService<Field>();
+			}
+			return m_Field;
+		}
+	}
+
 	public void FixedUpdate()
 	{
+		// Create movement vector according to input
 		var speed = Input.GetAxis("Vertical") * Speed;
-		var position = transform.position;
-		position.z += speed * Time.fixedDeltaTime;
-		transform.position = position;
+		var delta = speed * Time.fixedDeltaTime;
+
+		// Limit movement by field's bounds
+		var myBounds = collider.bounds;
+		var fieldBounds = Field.Bounds;
+		var maxDelta = fieldBounds.max.z - myBounds.max.z;
+		var minDelta = fieldBounds.min.z - myBounds.min.z;
+		delta = Mathf.Max(minDelta,delta);
+		delta = Mathf.Min(maxDelta,delta);
+
+		// Apply movement
+		var movement = new Vector3(0,0,delta);
+		transform.position += movement;
 	}
 }
