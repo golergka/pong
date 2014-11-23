53295b3f183f92c8c057e83148bd430938873deb
Max Yankov <golergka@gmail.com>
2014-11-23 13:33:19 +0200

    Stub game implementation

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : Service
+{
+	public void Score(Player _PlayerGoal)
+	{
+		Debug.Log("Score for " + _PlayerGoal);
+	}
+}
--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -3,8 +3,23 @@ using System.Collections;
 
 public class Goal : MonoBehaviour
 {
+	Game m_Game;
+	Game Game
+	{
+		get
+		{
+			if (m_Game == null)
+			{
+				m_Game = ServiceProvider.GetService<Game>();
+			}
+			return m_Game;
+		}
+	}
+	
+	public Player Player;
+
 	public void OnTriggerEnter(Collider _Other)
 	{
-		Debug.Log("Goal!");
+		Game.Score(Player);
 	}
 }
